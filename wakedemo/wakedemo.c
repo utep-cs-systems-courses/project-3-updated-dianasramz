2213644697c8ff8d11c1b5215046977a95d7a3b1
Eric Freudenthal <eric@freudenthal.net>
2020-11-03 13:03:32 -0700

    created wakedemo

---
+++ wakedemo/wakedemo.c
@@ -0,0 +1,51 @@
+#include <msp430.h>
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+
+#define LED_GREEN BIT6             // P1.6
+
+
+short redrawScreen = 1;
+u_int fontFgColor = COLOR_GREEN;
+
+void wdt_c_handler()
+{
+  static int secCount = 0;
+
+  secCount ++;
+  if (secCount == 250) {		/* once/sec */
+    secCount = 0;
+    fontFgColor = (fontFgColor == COLOR_GREEN) ? COLOR_BLACK : COLOR_GREEN;
+    redrawScreen = 1;
+  }
+}
+  
+
+void main()
+{
+  P1DIR |= LED_GREEN;		/**< Green led on when CPU on */		
+  P1OUT |= LED_GREEN;
+  configureClocks();
+  lcd_init();
+  
+  enableWDTInterrupts();      /**< enable periodic interrupt */
+  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
+  clearScreen(COLOR_BLUE);
+  while (1) {			/* forever */
+    if (redrawScreen) {
+      redrawScreen = 0;
+      drawString5x7(20,20, "hello", fontFgColor, COLOR_BLUE);
+    }
+    P1OUT &= ~LED_GREEN;	/* green off */
+    or_sr(0x10);		/**< CPU OFF */
+    P1OUT |= LED_GREEN;		/* green on */
+  }
+}
+
+    
+    
+
+
+
